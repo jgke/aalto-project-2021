b9c135f2a94e35d345ca7a54a927695b7f7bb991
Tommi Pakarinen <tommivinhpakarinen@gmail.com>
2021-11-01 16:39:00 +0200

    Boilerplate for front and back end

---
+++ src/server/route/path.ts
@@ -0,0 +1,12 @@
+import {Ipath, IPathRoute} from "../domain/IPath";
+
+function path(url: string): IPathRoute {
+    const allRoutes: Ipath = {
+        "/test": {
+            methods: ["POST", "GET", "PUT", "DELETE"]
+        }
+    }
+    return allRoutes[url];
+}
+
+export default path;
