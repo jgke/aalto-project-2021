05389ee1456135661cfadda06551ba0b5a7824d8
Tommi Pakarinen <tommivinhpakarinen@gmail.com>
2021-11-16 00:24:33 +0200

    pg improvements

--- src/server/dbConfigs.ts
+++ src/server/dbConfigs.ts
@@ -1,45 +1,32 @@
-import {configs} from './configs';
-import {Pool, PoolClient, QueryConfig} from 'pg';
-
-const pool = new Pool(configs.poolConfig);
-
-interface ExtendedPoolClient extends PoolClient {
-    lastQuery: any[];
-}
+import {getConfig} from './configs';
+import {Pool, PoolConfig, QueryConfig} from 'pg';
 
 class Database {
-    async query(text: string | QueryConfig<any>, params: any) {
-      const start = Date.now();
-      const res = await pool.query(text, params);
-      const duration = Date.now() - start;
-      console.log('executed query', { text, duration, rows: res.rowCount });
-      return res;
+	private _pool: Pool | null = null;
+
+    async query(text: string | QueryConfig<any>, params: unknown[]) {
+		const start = Date.now();
+		const pool = await this.getPool();
+		const res = await pool.query(text, params);
+		const duration = Date.now() - start;
+		console.log('executed query', { text, duration, rows: res.rowCount });
+		return res;
     }
 
     async getClient() {
-      const client = await pool.connect() as ExtendedPoolClient;
-    //   const query = client.query;
-    //   const release = client.release;
-    //   // set a timeout of 5 seconds, after which we will log this client's last query
-    //   const timeout = setTimeout(() => {
-    //     console.error('A client has been checked out for more than 5 seconds!');
-    //     console.error(`The last executed query on this client was: ${client.lastQuery}`);
-    //   }, 5000);
-    //   // monkey patch the query method to keep track of the last query executed
-    //   client.query = (...args) => {
-    //     client.lastQuery = args;
-    //     return query.apply(client, args);
-    //   }
-    //   client.release = () => {
-    //     // clear our timeout
-    //     clearTimeout(timeout);
-    //     // set the methods back to their old un-monkey-patched version
-    //     client.query = query;
-    //     client.release = release;
-    //     return release.apply(client);
-    //   }
-      return client;
+		const pool = await this.getPool();
+      	const client = await pool.connect();
+      	return client;
     }
-  }
 
-export default new Database();
+	async getPool(): Promise<Pool> {
+		if (!this._pool) {
+			const config = await getConfig();
+			this._pool = new Pool(config.poolConfig);
+		}
+		return this._pool;
+	}
+}
+
+const db = new Database();
+export { db };
