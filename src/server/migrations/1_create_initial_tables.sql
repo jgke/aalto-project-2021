9d8c69c04342b5efacf168d836644e29046dbdaa
Juhani Kesti <90926651+rendurok@users.noreply.github.com>
2021-11-25 16:05:24 +0200

    add database table for edges in migration

--- src/server/migrations/1_create_initial_tables.sql
+++ src/server/migrations/1_create_initial_tables.sql
@@ -9,3 +9,11 @@ CREATE TABLE node (
   x INTEGER NOT NULL,
   y INTEGER NOT NULL
 );
+
+CREATE TABLE edge (
+  source_id INTEGER REFERENCES node(id), 
+  target_id INTEGER REFERENCES node(id)
+);
+
+ALTER TABLE edge 
+ADD CONSTRAINT PK_edge PRIMARY KEY (source_id, target_id);
