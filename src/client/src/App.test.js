147c3eb56f81a98589806996fbfe7e15fb90908a
jannesp1 <paavo.jannes@aalto.fi>
2021-11-27 13:21:51 +0200

    fixed dependency issues and added a test file

---
+++ src/client/src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import App from './App';
+
+
+
+
+test('renders node', () => {
+    
+      
+    })
