fa0bc3999972450b2f82d3a45bb12dc663655108
Tenho Korhonen <tenho.korhonen@aalto.fi>
2021-11-27 15:56:18 +0200

    add try-catch to edges' post request

--- src/server/route/routes/edge.ts
+++ src/server/route/routes/edge.ts
@@ -12,13 +12,16 @@ router.route("/edge")
   .post(async (req: Request, res: Response) => {
     console.log("Receiving edge...");
     const text: IEdge = req.body; //Might have to parse this
-
-    const q = await db.query(
-      "INSERT INTO edge (source_id, target_id) VALUES ($1, $2)",
-      [text.source_id, text.target_id]
-    );
-
-    res.status(200).json(q);
+    try {
+      const q = await db.query(
+        "INSERT INTO edge (source_id, target_id) VALUES ($1, $2)",
+        [text.source_id, text.target_id]
+      );
+      res.status(200).json(q);
+    }
+    catch { (e: Error) => (
+      console.log(e)
+    )}
   })
   .put((req: Request, res: Response) => {
     res.status(501).json({ message: "Not implemented" });
