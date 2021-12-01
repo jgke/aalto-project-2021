96d02a8caaaa61d4690ab886b55d0e21d45fade8
jannesp1 <paavo.jannes@aalto.fi>
2021-12-01 16:21:54 +0200

    fixing

--- src/client/src/tests/App.test.js
+++ src/client/src/tests/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent, screen, debug } from '@testing-library/react';
-import App from '../App';
+import App from './App';
 
 
 test('renders with default props', () => {
- const { getByText } = render(<App/>);
+ const { getByText } = render( <App/> );
  const output = getByText('title:');
     
  expect(output).toHaveAttribute('name', 'Text')
