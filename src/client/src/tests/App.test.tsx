278652717d448cc8e8cc56bac92d4d5b40fb3ea6
jannesp1 <paavo.jannes@aalto.fi>
2021-12-02 15:36:43 +0200

    added basic tests for frontend

--- src/client/src/tests/App.test.tsx
+++ src/client/src/tests/App.test.tsx
@@ -7,12 +7,26 @@ import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import App from '../App';
 
+// test for submit button
+
 
 test('renders with default props', () => {
     const { getByText } = render( <App/> );
-    const output = getByText('Tasks');
+    const output1 = getByText('Tasks');
+    const output2 = getByText('Add task');
+    const output3 = getByText('Text:');
 
-    expect(output).toHaveTextContent('Tasks');
+    expect(output1).toHaveTextContent('Tasks');
+    expect(output2).toHaveTextContent('Add task');
+    expect(output3).toHaveTextContent('Text:');
  
-})
+});
+
+test('should display a blank input bar', async () => {
+    const { findByTestId } = render( <App/> );
+    const form = await findByTestId('nodetext');
+    
+    expect(form).toHaveValue('');
+    
+});
 
