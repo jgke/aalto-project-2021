3ddf307ad1b90aee86347a4e23bf666949584989
Tobi Falowo <tobifalowo@gmail.com>
2021-12-06 05:09:26 +0200

    tests for adding and removing nodes in client

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -9,3 +9,90 @@ describe('Test A', () => {
     cy.visit('http://localhost:3000')
   })
 })
+
+describe('Has nodetext', () => {
+  it('Has an input to enter node name', () => {
+    cy.get('input#nodetext')
+  })
+})
+
+describe('nodetext has add button', () => {
+  it('Has button to add node', () => {
+    cy.get('input#nodetext').parent().contains('Add')
+  })
+})
+
+describe('test add node', () => {
+  it('Can add nodes', () => {
+    let node_name_1 = '__test__1'
+    let node_name_2 = '__test__2'
+
+    // remove pre-existing nodes with the test name
+    cy.get("body").then(($body) => {
+      $body.find(`.react-flow__node-default:contains(${node_name_1})`).each((index, $div, $list) => {
+        cy.wrap($div).click('topLeft', { force: true })
+        cy.wait(100)
+        cy.get('body').trigger('keydown', { key: "Backspace", charCode: 0, keyCode: 8 })
+        cy.wait(50)
+        cy.get('body').trigger('keyup', { key: "Backspace", charCode: 0, keyCode: 8 })
+        cy.wait(50)
+      })
+      $body.find(`.react-flow__node-default:contains(${node_name_2})`).each((index, $div, $list) => {
+        cy.wrap($div).click('topLeft', { force: true })
+        cy.wait(100)
+        cy.get('body').trigger('keydown', { key: "Backspace", charCode: 0, keyCode: 8 })
+        cy.wait(50)
+        cy.get('body').trigger('keyup', { key: "Backspace", charCode: 0, keyCode: 8 })
+        cy.wait(50)
+      })
+    })
+
+    cy.wait(400)
+
+    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist')
+    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist')
+
+    cy.get('input#nodetext').type(node_name_1)
+    cy.get('input#nodetext').parent().contains('Add').click()
+
+    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist')
+
+    for (let i = 0; i < 3; i++) {
+      cy.get('input#nodetext').type(node_name_1)
+      cy.get('input#nodetext').parent().contains('Add').click()
+    }
+
+    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist')
+    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist')
+
+    cy.get('input#nodetext').type(node_name_2)
+    cy.get('input#nodetext').parent().contains('Add').click()
+
+    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist')
+    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('exist')
+
+    // remove pre-existing nodes with the test name
+    cy.get("body").then(($body) => {
+      $body.find(`.react-flow__node-default:contains(${node_name_1})`).each((index, $div, $list) => {
+        cy.wrap($div).click('topLeft', { force: true })
+        cy.wait(100)
+        cy.get('body').trigger('keydown', { key: "Backspace", charCode: 0, keyCode: 8 })
+        cy.wait(50)
+        cy.get('body').trigger('keyup', { key: "Backspace", charCode: 0, keyCode: 8 })
+        cy.wait(50)
+      })
+      $body.find(`.react-flow__node-default:contains(${node_name_2})`).each((index, $div, $list) => {
+        cy.wrap($div).click('topLeft', { force: true })
+        cy.wait(100)
+        cy.get('body').trigger('keydown', { key: "Backspace", charCode: 0, keyCode: 8 })
+        cy.wait(50)
+        cy.get('body').trigger('keyup', { key: "Backspace", charCode: 0, keyCode: 8 })
+        cy.wait(50)
+      })
+    })
+
+    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist')
+    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist')
+
+  })
+})
