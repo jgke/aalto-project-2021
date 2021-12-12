bae6f5ee39ab86beca06f5d644ac91da42299ea1
Pakarinen Tommi <tommi.pakarinen@aalto.fi>
2021-12-12 22:30:26 +0200

    Revert "jest front mocks"
    This reverts commit 77c71f2ca6818b73ef4635c2e187adc01b037bc2.

--- src/client/jest.config.js
+++ src/client/jest.config.js
@@ -9,16 +9,14 @@ module.exports = {
     // Jest transformations -- this adds support for TypeScript
     // using ts-jest
     transform: {
-      "^.+\\.tsx?$": "ts-jest",
-      ".(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest-config/file-mock.js",
-      '.(css|less)$': '<rootDir>/jest-config/style-mock.js'
+      "^.+\\.tsx?$": "ts-jest"
     },
   
     // Runs special logic, such as cleaning up components
     // when using React Testing Library and adds special
     // extended assertions to Jest
     setupFilesAfterEnv: [
-        "<rootDir>/jest-config/setupTests.ts"
+        "<rootDir>/src/setupTests.ts"
     ],
   
     // Test spec file resolution pattern
