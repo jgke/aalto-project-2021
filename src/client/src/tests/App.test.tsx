fc753dee64a8bb506a9318527f0ee637a16feb2b
Tenho Korhonen <tenho.korhonen@aalto.fi>
2022-01-14 13:58:01 +0200

    fix indentation

--- src/client/src/tests/App.test.tsx
+++ src/client/src/tests/App.test.tsx
@@ -10,29 +10,29 @@ import createNode from '../App';
 
 beforeAll(() => {
     window.ResizeObserver =
-      window.ResizeObserver ||
-      jest.fn().mockImplementation(() => ({
-        disconnect: jest.fn(),
-        observe: jest.fn(),
-        unobserve: jest.fn(),
-      }));
-  
+        window.ResizeObserver ||
+        jest.fn().mockImplementation(() => ({
+            disconnect: jest.fn(),
+            observe: jest.fn(),
+            unobserve: jest.fn(),
+        }));
+
     Object.defineProperties(window.HTMLElement.prototype, {
-      offsetHeight: {
-        get() {
-          return parseFloat(this.style.height) || 1;
+        offsetHeight: {
+            get() {
+                return parseFloat(this.style.height) || 1;
+            },
         },
-      },
-      offsetWidth: {
-        get() {
-          return parseFloat(this.style.width) || 1;
+        offsetWidth: {
+            get() {
+                return parseFloat(this.style.width) || 1;
+            },
         },
-      },
     });
     //eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (window.SVGElement as any).prototype.getBBox = () => ({x:0, y:0, width: 0, height: 0});
-  });
-  
+    (window.SVGElement as any).prototype.getBBox = () => ({ x: 0, y: 0, width: 0, height: 0 });
+});
+
 test('Renders with default props', () => {
     const { getByText } = render(<App />);
     const output1 = getByText('Tasks');
