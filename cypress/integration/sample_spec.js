97873680ef3ccc3ee21b19efdcfadcecc7ac93dc
Tobi Falowo <tobifalowo@gmail.com>
2022-01-17 05:07:37 +0200

    cypress -- function to delete all test nodes

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,127 +1,92 @@
 describe('Passing Dummy Test', () => {
-  it('Does not do much', () => {
-    expect(true).to.equal(true)
-  })
-})
+    it('Does not do much', () => {
+        expect(true).to.equal(true)
+    });
+});
 
 describe('Test A', () => {
-  it('Visit page', () => {
-    cy.visit('http://localhost:3000')
-  })
-})
+    it('Visit page', () => {
+        cy.visit('http://localhost:3000')
+    });
+});
 
 describe('Has nodetext', () => {
-  it('Has an input to enter node name', () => {
-    cy.get('input#nodetext')
-  })
-})
+    it('Has an input to enter node name', () => {
+        cy.get('input#nodetext')
+    });
+});
 
 describe('nodetext has add button', () => {
-  it('Has button to add node', () => {
-    cy.get('input#nodetext').parent().contains('Add')
-  })
-})
+    it('Has button to add node', () => {
+        cy.get('input#nodetext').parent().contains('Add')
+    });
+});
 
 describe('test add node', () => {
-  it('Can add and remove nodes', () => {
-    let node_name_1 = '__test__1'
-    let node_name_2 = '__test__2'
-    let node_n1 = 3;
+    it('Can add and remove nodes', () => {
+        let node_name_1 = '__test__1';
+        let node_name_2 = '__test__2';
+        let node_n1 = 3;
 
-    // remove pre-existing nodes with the test name
-    cy.get("body").then(($body) => {
-      $body.find(`.react-flow__node-default:contains(${node_name_1})`).each((index, $div, $list) => {
-        cy.removeNodeDiv(index, $div, $list);
-      });
-      $body.find(`.react-flow__node-default:contains(${node_name_2})`).each((index, $div, $list) => {
-        cy.removeNodeDiv(index, $div, $list);
-      });
+        // remove nodes prefixed with __test__
+        cy.removeAllTestNodes();
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist');
+        cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
+        
+        cy.insertNode(node_name_1);
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist');
+
+        for (let i = 0; i < node_n1; i++) {
+            cy.insertNode(node_name_1);
+        }
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist');
+        cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
+
+        cy.insertNode(node_name_2);
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist');
+        cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('exist');
+
+        cy.removeAllTestNodes();
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist');
+        cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
+
     });
-
-    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist');
-    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
-    
-    cy.insertNode(node_name_1);
-
-    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist');
-
-    for (let i = 0; i < node_n1; i++) {
-      cy.insertNode(node_name_1);
-    }
-
-    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist');
-    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
-
-    cy.insertNode(node_name_2);
-
-    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist');
-    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('exist');
-
-    // nodes with the test name
-    cy.get("body").then(($body) => {
-      $body.find(`.react-flow__node-default:contains(${node_name_1})`).each((index, $div, $list) => {
-        cy.removeNodeDiv(index, $div, $list);
-      })
-      $body.find(`.react-flow__node-default:contains(${node_name_2})`).each((index, $div, $list) => {
-        cy.removeNodeDiv(index, $div, $list);
-      })
-    })
-
-    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist');
-    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
-
-  })
-})
+});
 
 describe('test add edge', () => {
-  it('Can add and remove edges', () => {
-    let node_name_1 = '__test__1'
-    let node_name_2 = '__test__2'
+    it('Can add and remove edges', () => {
+        let node_name_1 = '__test__1';
+        let node_name_2 = '__test__2';
 
-    // remove pre-existing nodes with the test name
-    cy.get("body").then(($body) => {
-      $body.find(`.react-flow__node-default:contains(${node_name_1})`).each((index, $div, $list) => {
-        cy.removeNodeDiv(index, $div, $list);
-      });
-      $body.find(`.react-flow__node-default:contains(${node_name_2})`).each((index, $div, $list) => {
-        cy.removeNodeDiv(index, $div, $list);
-      });
+        cy.removeAllTestNodes();
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist');
+        cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
+        
+        cy.insertNode(node_name_1);
+        cy.insertNode(node_name_2);
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist');
+        cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('exist');
+
+        cy.get('.react-flow__edge-straight').should('have.length', 0);
+
+        cy.get(`.react-flow__node-default:contains(${node_name_2})`).find('.react-flow__handle-bottom').should('have.length', 1).trigger('mousedown');
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).find('.react-flow__handle-top').should('have.length', 1).trigger('mouseup');
+
+
+        cy.get('.react-flow__edge-straight').should('have.length', 1);
+
+        cy.removeAllTestNodes();
+
+        cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist');
+        cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
+
     });
-
-    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist');
-    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
-    
-    cy.insertNode(node_name_1);
-    cy.insertNode(node_name_2);
-
-    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('exist');
-    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('exist');
-
-    cy.get("body").then(($body) => {
-      cy.wrap($body).get('.react-flow__edge-straight').should('have.length', 0);
-
-      cy.wrap($body).get(`.react-flow__node-default:contains(${node_name_2})`).find('.react-flow__handle-bottom').should('have.length', 1).trigger('mousedown');
-
-      cy.wrap($body).get(`.react-flow__node-default:contains(${node_name_1})`).find('.react-flow__handle-top').should('have.length', 1).trigger('mouseup');
-
-    });
-
-    cy.get("body").then(($body) => {
-      cy.wrap($body).get('.react-flow__edge-straight').should('have.length', 1);
-    });
-
-    // remove added nodes
-    cy.get("body").then(($body) => {
-      $body.find(`.react-flow__node-default:contains(${node_name_1})`).each((index, $div, $list) => {
-        cy.removeNodeDiv(index, $div, $list);
-      });
-      $body.find(`.react-flow__node-default:contains(${node_name_2})`).each((index, $div, $list) => {
-        cy.removeNodeDiv(index, $div, $list);
-      });
-    });
-
-    cy.get(`.react-flow__node-default:contains(${node_name_1})`).should('not.exist');
-    cy.get(`.react-flow__node-default:contains(${node_name_2})`).should('not.exist');
-
-  })
-})
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,14 +24,36 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })W
 
+
+// cypress is able to click the nodes even if they're outside the bounds as long as they are not covered by another node
 Cypress.Commands.add('removeNodeDiv', (index, $div, $list) => {
     cy.wrap($div).click('topLeft', { force: true });
     cy.wrap($div).should('have.class', 'selected');
     cy.get('body').trigger('keydown', { key: "Backspace", charCode: 0, keyCode: 8 })
-      .trigger('keyup', { key: "Backspace", charCode: 0, keyCode: 8 });
+        .trigger('keyup', { key: "Backspace", charCode: 0, keyCode: 8 });
 });
 
 Cypress.Commands.add('insertNode', (nodeName) => {
+    var b_NodeNamePrefixed = true;
+
+    cy.get('input#nodetext').invoke('attr', 'value').should('eq', '');
     cy.get('input#nodetext').type(nodeName);
+    cy.get('input#nodetext').invoke('attr', 'value').should('eq', nodeName);
+
+    if (b_NodeNamePrefixed) {
+        // nodeName should be prefixed by '__test__'
+        expect(nodeName).match(/^__test__/);
+    }
     cy.get('input#nodetext').parent().contains('Add').click();
+});
+
+// remove nodes prefixed with __test__
+Cypress.Commands.add('removeAllTestNodes', () => {
+    var nodeNamePrefix = '__test__';
+
+    cy.get("body").then(($body) => {
+        $body.find(`.react-flow__node-default:contains(${nodeNamePrefix})`).each((index, $div, $list) => {
+            cy.removeNodeDiv(index, $div, $list);
+        });
+    });
 });
