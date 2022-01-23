a133e68ac252d4bb32d42342651d1e62508a77bf
Juhani Kesti <90926651+rendurok@users.noreply.github.com>
2022-01-23 15:24:43 +0200

    add dagre layouting

---
+++ src/client/src/services/layoutService.ts
@@ -0,0 +1,49 @@
+import { Elements, isNode, Position } from 'react-flow-renderer';
+import dagre from 'dagre';
+
+//copied almost directly from react flow documentation
+
+// In order to keep this example simple the node width and height are hardcoded.
+// In a real world app you would use the correct width and height values of
+// const nodes = useStoreState(state => state.nodes) and then node.__rf.width, node.__rf.height
+
+const nodeWidth = 300;
+const nodeHeight = 50;
+
+export const dagreLayout = (elements: Elements, direction = 'TB'): Elements => {
+    const dagreGraph = new dagre.graphlib.Graph();
+    dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+    const isHorizontal = direction === 'LR';
+    dagreGraph.setGraph({ rankdir: direction });
+
+    elements.forEach((el) => {
+        if (isNode(el)) {
+            dagreGraph.setNode(el.id, { width: nodeWidth, height: nodeHeight });
+        } else {
+            dagreGraph.setEdge(el.source, el.target);
+        }
+    });
+
+    dagre.layout(dagreGraph);
+
+    return elements.map((el) => {
+        if (isNode(el)) {
+            const nodeWithPosition = dagreGraph.node(el.id);
+            el.targetPosition = isHorizontal ? Position.Left : Position.Top;
+            el.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
+
+            // unfortunately we need this little hack to pass a slightly different position
+            // to notify react flow about the change. Moreover we are shifting the dagre node position
+            // (anchor=center center) to the top left so it matches the react flow node anchor point (top left).
+            el.position = {
+                x: nodeWithPosition.x - nodeWidth / 2 + Math.random() / 1000,
+                y: nodeWithPosition.y - nodeHeight / 2,
+            };
+        }
+
+        return el;
+    });
+};
+
+//copy-paste ends
