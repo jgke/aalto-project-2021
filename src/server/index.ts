70f61bd18a3b769351331112afd489a726daa2e5
Pakarinen Tommi <tommi.pakarinen@aalto.fi>
2022-01-27 15:45:04 +0200

    fixes server not working with BrowserRouter

--- src/server/index.ts
+++ src/server/index.ts
@@ -14,17 +14,18 @@ app.use(express.json() as RequestHandler);
 
 const port: number = Number(process.env.PORT) || 8050; // set our port
 
-// Send index.html on root request
-app.use(express.static('dist'));
-app.get('/', (req: Request, res: Response) => {
-    console.log('sending index.html');
-    res.sendFile('/dist/index.html');
-});
 
 // REGISTER ROUTES
 // all of the routes will be prefixed with /api
 const routes: Router[] = Object.values(router);
 app.use('/api', routes);
+
+// Send index.html on root request
+app.use(express.static('dist'));
+app.get('/*', (req: Request, res: Response) => {
+    console.log('sending index.html');
+    res.sendFile('/dist/index.html', { root : __dirname});
+});
 
 // START THE SERVER
 // =============================================================================
