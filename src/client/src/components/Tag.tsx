bb807d287d17311c4daac355c74ddcb1c980ed15
Tobi Falowo <tobifalowo@gmail.com>
2022-02-17 15:22:18 +0200

    move functionality to Tag.tsx

--- src/client/src/components/Tag.tsx
+++ src/client/src/components/Tag.tsx
@@ -2,21 +2,85 @@ import React, { FormEvent, useState } from 'react';
 import { Node } from 'react-flow-renderer';
 import { ITag, ITaggedNode } from '../../../../types';
 import { basicNode } from '../App';
+import * as tagService from '../services/tagService';
+
+import ReactFlow, {
+    ReactFlowProps,
+} from 'react-flow-renderer';
 
 interface TagProps {
-    data: ITag[];
-    createTag: (tagLabel: string) => void;
-    onTagEdit: (data: ITag) => void;
-    onTagRemove: (id: number) => void;
+    tags: ITag[];
+    setTags: React.Dispatch<React.SetStateAction<ITag[]>>
 }
 
 export const Tag = (props: TagProps): JSX.Element => {
+    const [hasInited, setInited] = useState<boolean>(false);
+
     const [tagLabel, settagLabel] = useState<string>('');
 
     const handleSubmit = (event: FormEvent) => {
-        props.createTag(tagLabel);
+        createTag(tagLabel);
         event.preventDefault();
     };
+
+    const createTag = async (tagLabel: string): Promise<void> => {
+        const t: ITag = {
+            id: 0,
+            label: tagLabel,
+            color: 'red',
+        };
+        const returnId: number | undefined = await tagService.sendTag(t);
+        if (returnId) {
+            t.id = returnId;
+            //setTags(tags.concat(t));
+            await refreshTagList();
+        }
+    };
+
+    const onTagEdit = async (data: ITag) => {
+        props.setTags((tgs) =>
+            tgs.map((tg) => {
+                if (tg.id === data.id) {
+                    tg = data;
+                }
+                return tg;
+            })
+        );
+
+        await tagService.updateTag(data);
+    }
+
+    const onTagRemove = async (id: number) => {
+        const idx = props.tags.findIndex( t => t.id === id );
+        if(idx >= 0){
+            console.log('deleting tag: ', props.tags[idx]);
+            await tagService.deleteTag(id);
+            //setTags(tags.splice(idx, 1));
+            await refreshTagList();
+        } else {
+            console.log('could not find tag with id: ', id);
+        }
+    }
+
+    const refreshTagList = async (): Promise<void> => {
+        try {
+            console.log('refreshing tag list');
+            const tagList = await tagService.getAll();
+            // TODO: limit the number of tags returned from tagService
+            //console.log(tagList[0].label)
+            props.setTags(tagList.slice(0, 49));
+        } catch (e) {
+            console.log('Error in tagService.getAll', e);
+        }
+    }
+
+    const init = async() => {
+        setInited(true);
+    }
+
+    if (!hasInited) {
+        init();
+    }
 
     return (
         <div className="tag-sidebar">
@@ -32,7 +96,7 @@ export const Tag = (props: TagProps): JSX.Element => {
                 />
             </form>
             <div className="tag-taglist">
-                {props.data.map((tag) => (
+                {props.tags.map((tag) => (
                     <p key={tag.id}>{tag.label}</p>
                 ))}
             </div>
