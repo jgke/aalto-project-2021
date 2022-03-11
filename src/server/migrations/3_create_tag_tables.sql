7a652d2ba53a673fb95a212e389a2b5b4a4b536a
Tobi Falowo <tobifalowo@gmail.com>
2022-03-11 03:15:32 +0200

    change migration id and solve id conflict

--- src/server/migrations/3_create_tag_tables.sql
+++ src/server/migrations/3_create_tag_tables.sql
@@ -1,11 +1,16 @@
 CREATE TABLE tag (
-  id SERIAL PRIMARY KEY,
+  id SERIAL,
   label TEXT NOT NULL,
-  color TEXT NOT NULL
+  color TEXT NOT NULL,
+  project_id INTEGER NOT NULL REFERENCES project(id) ON DELETE CASCADE,
+  PRIMARY KEY (id, project_id)
 );
 
 CREATE TABLE tagged_nodes (
   node_id INTEGER REFERENCES node(id) ON DELETE CASCADE,
-  tag_id INTEGER REFERENCES tag(id) ON DELETE CASCADE,
-  PRIMARY KEY (node_id, tag_id)
+  tag_id INTEGER,
+  project_id INTEGER NOT NULL REFERENCES project(id) ON DELETE CASCADE,
+  PRIMARY KEY (node_id, tag_id, project_id),
+  FOREIGN KEY (tag_id, project_id) REFERENCES tag(id, project_id) ON DELETE CASCADE
+
 );
