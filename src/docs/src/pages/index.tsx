f5d2856f034d8cf93ccea67ff2cdd9c377b36249
daniel <dansku110@gmail.com>
2022-03-21 20:28:36 +0200

    intro in documentation

--- src/docs/src/pages/index.tsx
+++ src/docs/src/pages/index.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import { HomepageFeatures } from '../components/HomepageFeatures';
 
 function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
     return (
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-                    <Link
-                        className="button button--secondary button--lg"
-                        to="/docs/intro"
-                    >
-                        Docusaurus Tutorial - 5min ⏱️
-                    </Link>
-                </div>
+            <div>
+                <h1 className="hero__title">
+                    Graph based task management software
+                </h1>
+                <h2>
+                    Since Jira feels a bit clunky and in Trello it is difficult
+                    to follow task dependencies the graph based task management
+                    software was created to solve these issues.
+                </h2>
+                <h2>
+                    The main function of this software is to able to manage your
+                    project better by being able to see things like task name,
+                    priority of the task, what is the current status of the task
+                    using simple to create nodes. Nodes can be connected to each
+                    other via edges showing the dependency between the two. An
+                    edge has an arrow representing, which task should be done
+                    first. All nodes can be freely moved in the graph that they
+                    are inserted in allowing for easy customization.
+                </h2>
             </div>
         </header>
     );
@@ -28,16 +32,12 @@ function HomepageHeader() {
 
 // eslint-disable-next-line import/no-default-export
 export default function Home(): JSX.Element {
-    const { siteConfig } = useDocusaurusContext();
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />"
+            title={'Task Management Documentation'}
+            description="Documentation of the software<head />"
         >
             <HomepageHeader />
-            <main>
-                <HomepageFeatures />
-            </main>
         </Layout>
     );
 }
