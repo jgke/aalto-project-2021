e64a069509d936a01909e59adddd9ab052eab4a9
Juhani <90926651+rendurok@users.noreply.github.com>
2022-03-23 13:16:07 +0200

    clean up user assign migrations

--- src/server/migrations/6_create_user_assign_table.sql
+++ src/server/migrations/6_create_user_assign_table.sql
@@ -1,5 +1,5 @@
-CREATE TABLE userAssign (
+CREATE TABLE users__node (
     users_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     node_id INTEGER REFERENCES node(id) ON DELETE CASCADE,
-    CONSTRAINT PK_userAssign PRIMARY KEY (users_id, node_id)
+    PRIMARY KEY (users_id, node_id)
 );
