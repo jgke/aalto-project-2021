898e48acb1858e0bed342141d68098d772ac2932
Tenho Korhonen <tenho.korhonen@aalto.fi>
2022-03-29 18:07:36 +0300

    prettier

--- src/docs/src/components/HomepageFeatures.tsx
+++ src/docs/src/components/HomepageFeatures.tsx
@@ -15,19 +15,20 @@ const FeatureList: FeatureItem[] = [
         image: '/img/homepage_graph2.png',
         description: (
             <>
-                These pages contain the documentation for <em>Depsee</em>: a graph-based
-                task management app.
-                <br></br><br></br>
+                These pages contain the documentation for <em>Depsee</em>: a
+                graph-based task management app.
+                <br></br>
+                <br></br>
                 The purpose of this app to make task management easier by
-                visualizing dependencies between tasks. The goal is to
-                provide a tool that is more flexible than traditional
-                card-based approaches but more focused than general-purpose
-                graph editors. Our app does exactly this while retaining
-                extensive built-in support for agile methods such as Scrum.
-                <br></br><br></br>
-                Our software is open-source and the code can be viewed at
-                Github from the link below. The app is deployed at
-                Heroku.
+                visualizing dependencies between tasks. The goal is to provide a
+                tool that is more flexible than traditional card-based
+                approaches but more focused than general-purpose graph editors.
+                Our app does exactly this while retaining extensive built-in
+                support for agile methods such as Scrum.
+                <br></br>
+                <br></br>
+                Our software is open-source and the code can be viewed at Github
+                from the link below. The app is deployed at Heroku.
             </>
         ),
     },
